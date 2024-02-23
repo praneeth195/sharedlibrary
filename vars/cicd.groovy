ae5a74cf972db82d3431315a8cb741b96034fdd2
praneeth195 <159081177+praneeth195@users.noreply.github.com>
2024-02-23 15:20:23 -0500

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,7 +8,7 @@ def mvnBuild()
 }
 def Deploy(jobname,ip,context)
 {
-  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.jar"
 }
 def contTesting(jobname)
 {
