c01f561a7f63af167a7d634e64c20caec964eec9
praneeth <praneeth.sree195@gmail.com>
2024-02-23 15:40:14 -0500

    d

---
+++ cicd.groovy
@@ -0,0 +1,16 @@
+def gitDownload(repo)
+{
+  git "https://github.com/intelliqittrainings/${repo}.git"
+}
+def mvnBuild()
+{
+  sh "mvn package"
+}
+def Deploy(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
+def contTesting(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
